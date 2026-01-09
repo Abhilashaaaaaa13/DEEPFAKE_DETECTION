770b2361b38b7a69eeeeafd22ca3ee290ddd9334
Dhruv Barthwal <dhruv4216h@gmail.com>
2026-01-09 21:27:17 +0530

    Added Docker file

--- df_detection/src/App.tsx
+++ df_detection/src/App.tsx
@@ -5,18 +5,20 @@ import "./index.css";
 function App() {
   return (
     <div
-      className="h-[300px] w-[350px]
-      bg-gray-900                                 
-      p-4                              
+      className="relative min-h-[280px] w-[350px]
+      bg-gray-900
+      p-4
       flex flex-col gap-4"
     >
+      {/* ✨ Glow Effect */}
       <div
-        className="absolute -top-24 left-1/2 -translate-x-1/2
+        className="pointer-events-none absolute -top-24 left-1/2 -translate-x-1/2
         w-[420px] h-[220px]
         rounded-b-full
         bg-gradient-to-b from-pink-200/40 via-pink-100/20 to-transparent
         blur-3xl"
       />
+
       <Navbar />
       <Hero />
     </div>
